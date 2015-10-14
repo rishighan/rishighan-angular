69e166576a5d236c6ced69ca47fa520fef8ef244
Rishi Ghan <rishi.ghan@gmail.com>
2015-10-13 22:26:40 -0400

    added files

---
+++ app/components/admin/admin.component.js
@@ -0,0 +1,17 @@
+import template from './admin.html';
+import controller from './admin.controller';
+
+let adminComponent = function(){
+    return{
+        scope:{
+
+        },
+        restrict: 'E',
+        template,
+        controller,
+        controllerAs: 'adminc',
+        bindToController: true
+    }
+}
+
+export default adminComponent;
