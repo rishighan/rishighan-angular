6b1df27219941d6081874e7aabadd377b079af7f
Rishi Ghan <rishi.ghan@gmail.com>
2015-10-28 07:38:53 -0400

    Adding dropzone.js support to forms

--- app/components/admin/admin.controller.js
+++ app/components/admin/admin.controller.js
@@ -1,9 +1,44 @@
 class AdminController {
-    constructor($scope) {
+    constructor($scope, formlyValidationMessages) {
         this.gatirodhak = 'Ibn Batuta';
 
 
         this.postFormModel = {};
+        // dropzone config
+        $scope.dropzoneConfig = {
+            'options': {
+                url: '/api/files',
+                maxFileSize: 100,
+                paramName: 'uploadedFile',
+                maxThumbnailFilesize: 5,
+                autoProcessQueue: false,
+                maxFiles: 5
+            },
+            'eventHandlers': {
+                'sending': function(file, xhr, formData) {},
+                'success': function(file, response) {},
+                'maxfilesexceeded': function(file) {
+                    //alert("No more files please!");
+                    this.removeFile(file);
+                },
+                'addedfile': function(file) {
+                    //THIS IS WHERE THE MAGIC HAPPENS! we change the model and
+                    // tell angular to re-render the form and stuff. NOICE!
+
+                    this.postFormModel.flag = file.name;
+                    $scope.$digest();
+                }
+            }
+        };
+        // validation
+        this.options = {};
+
+        formlyValidationMessages.addTemplateOptionValueMessage('maxlength', 'maxlength', '', 'is the maximum length', 'Too long');
+        formlyValidationMessages.addTemplateOptionValueMessage('minlength', 'minlength', '', 'is the minimum length', 'Too short');
+        formlyValidationMessages.messages.required = 'to.label + " is required"';
+        formlyValidationMessages.messages.email = '$viewValue + " is not a valid email address"';
+
+        // form model
         this.postFormFields = [{
             type: 'input',
             key: 'postTitle',
@@ -16,6 +51,15 @@ class AdminController {
             templateOptions: {
                 label: 'Content'
             }
+        }, {
+            key: 'flag',
+            type: 'customInput',
+            templateOptions: {
+                label: 'Flag',
+                type: 'input',
+                placeholder: 'Flag image',
+                required: true
+            },
         }];
     }
 }
--- app/components/admin/admin.js
+++ app/components/admin/admin.js
@@ -2,11 +2,13 @@ import angular from 'angular';
 import uiRouter from 'ui-router';
 import formly from 'angular-formly';
 import apiCheck from 'api-check';
+import dropzone from 'dropzone';
 import adminComponent from './admin.component';
 
 let adminModule = angular.module('admin', [
     uiRouter,
-    formly
+    formly,
+
 ])
 
 .config(($stateProvider, $urlRouterProvider) => {
@@ -27,7 +29,47 @@ let adminModule = angular.module('admin', [
         name: 'textarea',
         template: '<label>{{options.templateOptions.label}}</label><textarea ng-model="model[options.key]" rows="10" cols="40"/></label>'
     });
+    formlyConfig.setType({
+        name: 'customInput',
+        extends: 'input',
+        controller: ['$scope',
+            function($scope) {
+                $scope.options.data.getValidationMessage = getValidationMessage;
+
+                function getValidationMessage(key) {
+                    var message = $scope.options.validation.messages[key];
+                    if (message) {
+                        return message($scope.fc.$viewValue, $scope.fc.$modelValue, $scope);
+                    }
+                }
+            }
+        ]
+    });
+    formlyConfig.setType({
+        name: 'maskedInput',
+        extends: 'customInput',
+        defaultOptions: {
+            ngModelAttrs: { // this is part of the magic... It's a little complex, but super powerful
+                mask: { // the key "ngMask" must match templateOptions.ngMask
+                    attribute: 'mask' // this the name of the attribute to be applied to the ng-model in the template
+                },
+                // applies the 'clean' attribute with the value of "true"
+                'true': {
+                    value: 'clean'
+                }
+            },
+            // this is how you hook into formly's messages API
+            // however angular-formly doesn't ship with ng-messages.
+            // You have to display these messages yourself.
+            validation: {
+                messages: {
+                    mask: '"Invalid input"'
+                }
+            }
+        }
+    });
 })
+
 
 .directive('admin', adminComponent);
 
