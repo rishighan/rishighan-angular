6594b6c4f9c7c9bb619918e3ec9e8791d31717a5
Rishi Ghan <rishi.ghan@gmail.com>
2015-11-08 13:51:37 -0500

    Added slack integration for Travis

--- tests/analytics.service.test.js
+++ tests/analytics.service.test.js
@@ -1,16 +1,15 @@
 "use strict";
 describe('Hello', function() {
 
-    beforeEach(module('rgApp'));
-    beforeEach(inject(function (_analyticsService_, $httpBackend) {
-        analyticsService = _analyticsService_;
-        httpBackend = $httpBackend;
-  }));
-
+    beforeEach(module('analytics'));
+    var _analyticsService;
+beforeEach(inject(['analyticsService', function (gaService) {
+    _analyticsService = gaService;
+}]));
 
 
     it('Should say analytics welcome message', function() {
         // spyOn(analyticsService, 'spawnAnalytics').andReturn('hello');
-        expect(_analyticsService_.spawnAnalytics).toBe("Booyah. Spawning Analytics...");
+        expect(_analyticsService.spawnAnalytics).toBe("Booyah. Spawning Analytics...");
     });
 });
