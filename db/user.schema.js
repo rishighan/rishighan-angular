c2da1d5d400242f75c71cc28a5c84943a226e603
Rishi Ghan <rishi.ghan@gmail.com>
2016-04-22 07:50:41 -0400

    user auth methods

--- db/user.schema.js
+++ db/user.schema.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
+var bcrypt = require('bcrypt');
+// var bindings = require('bindings')('binding.node');
 
 var UserSchema = new Schema({
     username: String,
@@ -8,5 +10,10 @@ var UserSchema = new Schema({
 });
 
 UserSchema.plugin(passportLocalMongoose);
-
+UserSchema.methods.verifyPassword = function(password, cb) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 module.exports = mongoose.model('UserSchema', UserSchema);
