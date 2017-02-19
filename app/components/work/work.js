77e5118410add051b54ffccdfa3f49050265b1c0
Rishi Ghan <rishi.ghan@gmail.com>
2017-02-18 22:17:53 -0500

    Added a single route for projects, cleaned up styles for Illustrations and markdown preview in Admin section

--- app/components/work/work.js
+++ app/components/work/work.js
@@ -1,8 +1,18 @@
 import workComponent from './work.component';
 
 let workModule = angular.module('work', [
-  'ui.router'
+    'ui.router'
 ])
-.directive('work', workComponent);
+    .config(($stateProvider) => {
+        $stateProvider
+            .state('project', {
+                url: '/work/:slug',
+                template: '<single></single>',
+                params: {id: undefined},
+                access: {restricted: false}
+            });
+    })
+    .directive('work', workComponent);
+
 
 export default workModule;
