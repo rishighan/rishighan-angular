20860dd018d370cd8a4b2c91fb4ec4e7bc7f639a
Rishi Ghan <rishi.ghan@gmail.com>
2017-06-19 07:28:32 -0400

    Codeship deployment configuration

--- app/shared/utils/domhelper.service.js
+++ app/shared/utils/domhelper.service.js
@@ -5,15 +5,15 @@ class DomHelperService {
     }
 
     collectionHas(a, b) {
-        for (var i = 0, len = a.length; i < len; i++) {
+        for (let i = 0, len = a.length; i < len; i++) {
             if (a[i] == b) return true;
         }
         return false;
     }
 
     findParentBySelector(elm, selector) {
-        var all = document.querySelectorAll(selector);
-        var cur = elm.parentNode;
+        let all = document.querySelectorAll(selector);
+        let cur = elm.parentNode;
         while (cur && !this.collectionHas(all, cur)) { //keep going up until you find a match
             cur = cur.parentNode; //go up
         }
