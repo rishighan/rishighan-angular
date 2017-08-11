5627ec3af2838206afc0a360d8a3be1784c8594d
rishi ghan <rishi.ghan@gmail.com>
2017-08-11 14:49:04 -0400

    Started work on trending posts

--- app/components/admin/posts/allposts.controller.js
+++ app/components/admin/posts/allposts.controller.js
@@ -14,7 +14,32 @@ class AllPostsController {
         PostService.getPosts($scope.pagerDefaults.page, $scope.pagerDefaults.pageSize)
             .then((posts) => {
                 $scope.posts = posts.data;
+                // Analytics for trending posts
+                // get data for each slug
+                // {x: 12, y: 2016-11-2, slug: 'sinking-in-the-quicksand'}
+                console.log($scope.posts)
+                _.each($scope.posts.docs, (post)  => {
+                    AnalyticsService.getAnalytics({slug: post.slug})
+                        .then((data) => {
+                            console.log(post);
+                            post.pageviews = $scope.calculatePageViews(data)
+                        })
+                });
+            })
+
+
+        $scope.analyticsData = [];
+
+
+        $scope.calculatePageViews = function (result) {
+            return _.map(result.data.rows, function (item) {
+                // data -> [{x: 1, y: 123},  {x: 123, y: 132}]
+                return {
+                    x: moment(item[0]).format("MMM Do YY"),
+                    y: parseInt(item[2], 10)
+                };
             });
+        };
 
         $scope.getMore = function (page, pageOffset) {
             PostService.getPosts(page, pageOffset)
@@ -38,24 +63,7 @@ class AllPostsController {
         };
         $scope.search = _.debounce($scope.searchPost, 500);
 
-        $scope.analyticsData = [];
-        // get data for each slug
-        // {x: 12, y: 2016-11-2, slug: 'sinking-in-the-quicksand'}
-        $scope.query = {
-            slug: 'configuring-clover-for-handoff-and-continuity-in-yosemite'
-        };
-        //get analytics
-        $scope.analyticsPromise = AnalyticsService.getAnalytics($scope.query).then(function (result) {
-            $scope.analyticsData = _.map(result.data.rows, function (item) {
-                // data -> [{x: 1, y: 123},  {x: 123, y: 132}]
-                return {
-                    x: moment(item[0]).format("MMM Do YY"),
-                    y: parseInt(item[2], 10)
-                };
-            });
-        }, function (error) {
-            $scope.analyticsData = {};
-        });
+
     }
 }
 export default AllPostsController;
