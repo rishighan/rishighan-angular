91f8dd7883b804b44477c2fa2d3833a9685e9c65
Rishi Ghan <rishi.ghan@yodle.com>
2017-08-19 22:40:14 -0400

    Custom cg-busy message for analytics

--- tests/unit/analyticsService.spec.js
+++ tests/unit/analyticsService.spec.js
@@ -1,15 +1,24 @@
-"use strict";
-describe('AnalyticsService Tests', function() {
-    var analyticsService;
+describe('AnalyticsService Tests', function () {
+    let analyticsService;
+    let analyticsQueryConfig = {
+        'ids': 'ga:17894417',
+        'start-date': '30daysAgo',
+        'end-date': 'yesterday',
+        'metrics': 'ga:pageviews',
+        'dimensions': 'ga:date, ga:pageTitle',
+        'filter': 'ga:pagePath=~/post/*',
+    };
     beforeEach(module('app.shared'));
 
-    beforeEach(inject(function(_AnalyticsService_) {
+    beforeEach(inject(function (_AnalyticsService_) {
         analyticsService = _AnalyticsService_;
     }));
 
 
-    it('Should say analytics welcome message', function() {
+    it('Should say analytics welcome message', function () {
         // debugger
         expect(analyticsService.spawnAnalytics()).toBe('This be Analytics.');
     });
+
+
 });
