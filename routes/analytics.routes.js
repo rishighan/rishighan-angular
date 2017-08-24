5b082781f522f849aedd08709b72729d10c76583
rishi ghan <rishi.ghan@gmail.com>
2017-08-24 12:41:09 -0400

    Manual bower install step

--- routes/analytics.routes.js
+++ routes/analytics.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Q = require('q');
 const google = require('googleapis');
-const key = require('../config/key.json');
+const key = require('../config/googleapi.key.json');
 const _ = require('underscore');
 const winston = require('winston');
 require('winston-loggly-bulk');
