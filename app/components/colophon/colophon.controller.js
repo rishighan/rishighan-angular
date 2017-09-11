c3742045748f47cee59bc4b77a24ed1352ad06e7
Rishi Ghan <rishi.ghan@yodle.com>
2017-09-10 20:55:33 -0400

    Added JSON L data to colophon

--- app/components/colophon/colophon.controller.js
+++ app/components/colophon/colophon.controller.js
@@ -1,19 +1,32 @@
 class ColophonController {
-    constructor(PostService){
+    constructor(PostService,
+                $translate) {
         this._PostService = PostService;
+        this._$translate = $translate;
         this.colophonData = {};
-
+        this.colophonJsonLd = {};
         this.fetchColophon();
     }
 
-    setColophonData(data){
+    setColophonData(data) {
         this.colophonData = data;
     }
 
-    fetchColophon(){
+    setColophonJsonLd(jsonLd) {
+        this.colophonJsonLd = jsonLd;
+    }
+
+    fetchColophon() {
         this._PostService.getPostsByTagName('colophon', 1, 1)
             .then((data) => {
                 this.setColophonData(data.data.docs);
+                this.setColophonJsonLd({
+                    "@context": "http://schema.org/",
+                    "@type": "Person",
+                    "name": this._$translate.instant('colophon.author_name.fragment'),
+                    "jobTitle": this._$translate.instant('colophon.job_title.fragment'),
+                    "url": this._$translate.instant('colophon.website.fragment')
+                });
             });
     }
 }
